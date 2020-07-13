4d4575d47210b62c288c199b0ae43ba25054aae0
Dan Taeyoung <dan@dantaeyoung.com>
2020-07-13 01:35:04 -0400

    working

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AirtableDisplayExample :records="records" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AirtableDisplayExample from "./components/AirtableDisplayExample.vue";
+
+const process_api_key = process.env.VUE_APP_AIRTABLE_API_KEY;
+const airtable_table = process.env.VUE_APP_AIRTABLE_TABLE;
+const airtable_base = process.env.VUE_APP_AIRTABLE_BASE;
+const airtable_view = process.env.VUE_APP_AIRTABLE_VIEW ;
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    AirtableDisplayExample
+  },
+  data: function() {
+    return {
+      records: []
+    };
+  },
+  created() {
+    this.loadAirtableData();
+  },
+  methods: {
+    loadAirtableData() {
+      var Airtable = require("airtable");
+      var base = new Airtable({ apiKey: process_api_key }).base(airtable_base);
+      let records = [];
+      var self = this;
+
+      base(airtable_table)
+        .select({
+          view: airtable_view,
+        })
+        .eachPage(
+          function page(partialRecords, fetchNextPage) {
+            records = [...records, ...partialRecords]
+            fetchNextPage();
+          },
+          function done(err) {
+            console.log("done", records);
+            self.records = records;
+            if (err) {
+              console.error(err);
+              return;
+            }
+          }
+        );
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -21,7 +60,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/AirtableDisplayExample.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="hello">
+    <div class="headers">
+      <header v-for="field in records[0].fields" :key="field">
+        {{ field }}
+      </header>
+    </div>
+    <div class="entries">
+      <div class="entry" v-for="record in records" v-bind:key="record">
+        <div
+          class="field left"
+          v-for="field in Object.keys(record.fields)"
+          :key="field"
+        >
+          {{ record.fields[field] }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AirtableDisplayExample",
+  props: ["records"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.headers {
+  font-style: italic;
+  margin: 1vh 0;
+  padding: 1vh 0;
+  border-bottom: 1px solid;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  align-content: flex-start;
+  justify-content: space-between;
+}
+
+header {
+  height: auto;
+  flex-basis: 20%;
+  text-align: left;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ul, p {
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  list-style: none;
+}
+
+.entries {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.entry {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  align-content: flex-start;
+  justify-content: space-between;
+  margin: 0.3vh 0.1vw;
+}
+
+.field {
+  height: auto;
+  flex-basis: 25%;
+  overflow: scroll;
+}
+
+
+</style>
